c544f1f08d4e6d59f0a6452c6bad4ff0e8b36112
Dusty Zietz <dustyzietz@gmail.com>
2019-10-08 19:48:41 -1000

    Updates

---
+++ precache-manifest.0218cfaf1aad77e98e8d32757973f647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec09be94c06002a1094eb5cfecce3ac",
+    "url": "/nomad/index.html"
+  },
+  {
+    "revision": "6fb11637eb43d760f66c",
+    "url": "/nomad/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d30c0b991ced90883cd2",
+    "url": "/nomad/static/js/2.c287fa20.chunk.js"
+  },
+  {
+    "revision": "6fb11637eb43d760f66c",
+    "url": "/nomad/static/js/main.3ba39e7a.chunk.js"
+  },
+  {
+    "revision": "0a32f63102e2a7a477be",
+    "url": "/nomad/static/js/runtime~main.08640347.js"
+  }
+]);
